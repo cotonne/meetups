491eb8c9d27344292b4d6c6e9e7e15771142d1e6
Laurent Yovanovitch <laurent.yovanovitch@outlook.fr>
2018-03-14 21:07:33 +0100

    BookingDao

---
+++ secureByDesign/netflix2/csharp/Netflix2.tests/BookingDaoTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Netflix2.repository;
+using NFluent;
+using NUnit.Framework;
+
+namespace Netflix2.tests
+{
+    [TestFixture]
+    public class BookingDaoTests
+    {
+        [Test]
+        public void Book_Ok()
+        {
+            var userId = "User" + Guid.NewGuid();
+            UserDao.Register(userId, "pwd");
+            var actual = BookingDao.Book("3", 10, userId);
+            Check.That(actual).IsTrue();
+        }
+    }
+}
