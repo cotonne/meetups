afcfe7431c7a049970eb9807339343734cb2be31
Laurent Yovanovitch <laurent.yovanovitch@outlook.fr>
2018-03-14 20:16:02 +0100

    Create DB, UserDao

---
+++ secureByDesign/netflix2/csharp/Netflix2.tests/UserDaoTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Netflix2.repository;
+using NFluent;
+using NUnit.Framework;
+
+namespace Netflix2.tests
+{
+    [TestFixture]
+    public class UserDaoTests
+    {
+        [Test]
+        public void Test1()
+        {
+            var result = UserDao.Register("toto", "pwd");
+            Check.That(result).IsEqualTo(true);
+        }
+    }
+}
---
+++ secureByDesign/netflix2/csharp/Netflix2/repository/UserDao.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netflix2.repository
+{
+    public class UserDao
+    {
+        /**
+         * Register a user to the system.
+         * The user identifier is unique.
+         *
+         * @param userId identifier of a user
+         * @param password password
+         * @return true if the user has been successfully registered, false otherwise
+         */
+        public static bool Register(string userId, string password)
+        {
+            return DB.Insert($"INSERT INTO USERS(ID, PASSWORD) values('{userId}', '{password}')");
+        }
+
+    }
+
+    public static class DB
+    {
+        public static bool Insert(string query)
+        {
+            using (var cnx = GetConnection())
+            {
+                cnx.Open();
+                using (var cmd = cnx.CreateCommand())
+                {
+                    cmd.CommandText = query;
+                    var result = cmd.ExecuteNonQuery();
+                    return result == 1;
+                }
+            }
+        }
+
+        private static SQLiteConnection GetConnection()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["Netflix2"].ConnectionString;
+            return new SQLiteConnection(connectionString);
+        }
+    }
+}
