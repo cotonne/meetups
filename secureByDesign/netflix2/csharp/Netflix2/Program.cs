16fe1608e792905cc344b9b87bb48490478958a6
Laurent Yovanovitch <laurent.yovanovitch@outlook.fr>
2018-03-17 12:14:41 +0100

    fix(Netflix2): display euro symbol in console

--- secureByDesign/netflix2/csharp/Netflix2/Program.cs
+++ secureByDesign/netflix2/csharp/Netflix2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Netflix2
 {
@@ -6,6 +7,8 @@ namespace Netflix2
     {
         static void Main(string[] args)
         {
+            ConfigureConsoleToDisplayEuroSymbol();
+
             Console.WriteLine("Registration ---- ");
             Console.WriteLine("Identifier : ");
             var userId = Console.ReadLine();
@@ -36,11 +39,16 @@ namespace Netflix2
 
                 Booking(dvdId, quantity, userId);
 
-                Console.WriteLine($"Nombre de DVD : {quantity} , prix final: 8eur x {quantity} DVD = {quantity * 8}" +
-                    $", Si vous en prenez un de plus : 7eur x {quantity + 1} DVD = {(quantity + 1) * 7} eur");
+                Console.WriteLine($"Nombre de DVD : {quantity} , prix final: 8€ x {quantity} DVD = {quantity * 8}€" +
+                    $", Si vous en prenez un de plus : 7€ x {quantity + 1} DVD = {(quantity + 1) * 7}€");
             }
 
             Console.ReadLine();
+        }
+
+        private static void ConfigureConsoleToDisplayEuroSymbol()
+        {
+            Console.OutputEncoding = Encoding.GetEncoding(1252);
         }
 
         private static void Register(string userId, string password)
